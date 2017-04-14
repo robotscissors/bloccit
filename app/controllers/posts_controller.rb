d585d5fae112d9ed218c0abcebe53f1a55835017
robotscissors <cjbazin@gmail.com>
2017-04-13 21:37:42 -0700

    Completed assignment-18

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    index = 0
+    @posts.each do |spamNotice|
+      if index % 5 == 0
+        spamNotice.title = "SPAM"
+      end
+      index += 1
+    end
   end
 
   def show
