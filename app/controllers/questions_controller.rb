84a183cd7650cfda6dd89f7433c2b1051e818d7e
robotscissors <cjbazin@gmail.com>
2017-04-18 22:26:30 -0700

    Move to Work assignment-20

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def new
+    @question = Question.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'questions/new'
+
+  get 'questions/create'
+
+  get 'questions/edit'
+
+  get 'questions/update'
+
+  get 'questions/destroy'
+
+  get 'questions/show'
+
+  resources :questions
   resources :posts
 
   get 'about' => 'welcome#about'
