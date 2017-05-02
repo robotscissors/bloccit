957cd4d6480ab76c5ce9bf309e7b50016f9cfd04
robotscissors <cjbazin@gmail.com>
2017-05-01 20:24:46 -0700

    Checkpoint 30 - completed.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,14 @@ Rails.application.routes.draw do
   end
   resources :posts, only: [] do
        resources :comments, only: [:create, :destroy]
+       post '/up-vote' => 'votes#up_vote', as: :up_vote
+       post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+
+
 
   get 'about' => 'welcome#about'
   root 'welcome#index'
